7b21997b62ed9b1cf0feafd26d8dc5d9d27b53a9
Marc Ernstberger <marc@marc1307.de>
2021-08-26 12:07:12 +0200

    improved config handling

--- app/config.py
+++ app/config.py
@@ -1,8 +1,46 @@
+import configparser, os
+import os.path
+from sys import path
+
+keysToImport = ['cf-key', 'cf-domain', 'ts-key', 'ts-tailnet']
+
+def importkey(name):
+    key = name
+
+    secretPath = "/run/secrets/"+key
+    if (os.path.isfile(secretPath)):
+        secret = open("secretPath", "r")
+        return secret.readlines()
+    elif (key in os.environ):
+        return os.environ.get(key)
+    else:
+        try:
+            cfgPath = os.path.dirname(os.path.realpath(__file__))+'/config.ini'
+            print(cfgPath)
+            with open(path, 'r') as file:
+                config = configparser.ConfigParser()
+                config.read(cfgPath)
+                cfg=config['DEFAULT']
+        except:
+            exit('could not read config file')
+        finally:
+            try:
+                out = cfg[key]
+                return out
+            except:
+                exit('could not you read config key')
+    
 def getConfig():
-    static = {
-        'cf-key': '',
-        'cf-domain': "".lower(),
-        'ts-key': 'tskey-',
-        'ts-tailnet': ''
-    }
+    # static = {
+    #     'cf-key': '',
+    #     'cf-domain': "".lower(),
+    #     'ts-key': 'tskey-',
+    #     'ts-tailnet': ''
+    # }
+    static = {}
+    for key in keysToImport:
+        static[key] = importkey(key)
     return static
+
+if __name__ == '__main__':
+    print(getConfig())
