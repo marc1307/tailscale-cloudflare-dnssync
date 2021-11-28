9d22daae4de1b2fac28cccbb4b668720c67f7662
Marc Ernstberger <marc@marc1307.de>
2021-11-28 02:47:38 +0100

    - added support for optional config parameters - fixed #3

--- app/config.py
+++ app/config.py
@@ -3,32 +3,37 @@ import os.path
 from sys import path
 
 keysToImport = ['cf-key', 'cf-domain', 'ts-key', 'ts-tailnet']
+keysOptional = ['prefix', 'postfix']
 
-def importkey(name):
+def importkey(name, optional=False):
     key = name
 
     secretPath = "/run/secrets/"+key
     if (os.path.isfile(secretPath)):
-        secret = open("secretPath", "r")
-        return secret.readlines()
+        secret = open(secretPath, "r")
+        out = "{}".format(secret.readline().strip())
+        return out
     elif (key in os.environ):
         return os.environ.get(key)
     else:
         try:
             cfgPath = os.path.dirname(os.path.realpath(__file__))+'/config.ini'
-            print(cfgPath)
             with open(path, 'r') as file:
                 config = configparser.ConfigParser()
                 config.read(cfgPath)
                 cfg=config['DEFAULT']
         except:
+            if optional:
+                return ""
             exit('could not read config file')
         finally:
             try:
                 out = cfg[key]
                 return out
             except:
-                exit('could not you read config key')
+                if optional:
+                    return ""
+                print("meh2")
     
 def getConfig():
     # static = {
@@ -40,6 +45,8 @@ def getConfig():
     static = {}
     for key in keysToImport:
         static[key] = importkey(key)
+    for key in keysOptional:
+        static[key] = importkey(key, True)
     return static
 
 if __name__ == '__main__':
