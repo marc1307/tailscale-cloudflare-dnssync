08f088cfb03507b89a151401ad357a65c115ecd7
marc1307 <github@marc1307.de>
2022-12-11 23:14:39 +0100

    updated record cleanup procedure

--- app/app.py
+++ app/app.py
@@ -26,16 +26,16 @@ def main():
     for ts_rec in ts_records: 
         #if ts_rec['hostname'] in cf_recordes['name']:
         if config.get("cf-sub"):
-            sub = "." + config.get("cf-sub")
+            sub = "." + config.get("cf-sub").lower()
         else:
             sub = ""
         tsfqdn = ts_rec['hostname'].lower()+sub+"."+config['cf-domain']
         if any(c['name'] == tsfqdn and c['content'] == ts_rec['address'] for c in cf_recordes):
-            print("[{state}]: {host} -> {ip}".format(host=ts_rec['hostname'], ip=ts_rec['address'], state=colored("FOUND", "green")))
+            print("[{state}]: {host} -> {ip}".format(host=tsfqdn, ip=ts_rec['address'], state=colored("FOUND", "green")))
         else:
             ip = ipaddress.ip_address(ts_rec['address'])
             if isValidDNSRecord(ts_rec['hostname']):
-                print("[{state}]: {host} -> {ip}".format(host=ts_rec['hostname'], ip=ts_rec['address'], state=colored("ADDING", "yellow")))
+                print("[{state}]: {host} -> {ip}".format(host=tsfqdn, ip=ts_rec['address'], state=colored("ADDING", "yellow")))
                 createDNSRecord(config['cf-key'], config['cf-domain'], ts_rec['hostname'], records_typemap[ip.version], ts_rec['address'],subdomain=config["cf-sub"])
             else:
                 print("[{state}]: {host}.{tld} -> {ip} -> (Hostname: \"{host}.{tld}\" is not valid)".format(host=ts_rec['hostname'], ip=ts_rec['address'], state=colored("SKIPING", "red"), tld=config['cf-domain']))
@@ -53,7 +53,7 @@ def main():
 
     for cf_rec in cf_recordes:
         if config.get('cf-sub'):
-            sub = '.' + config.get('cf-sub')
+            sub = '.' + config.get('cf-sub').lower()
         else: sub = ""
         cf_name = cf_rec['name'].rsplit(sub + '.' + config['cf-domain'], 1)[0]
 
@@ -63,14 +63,22 @@ def main():
         if not cf_name.endswith(config.get('postfix', '')):
             continue
 
+        # Ignore any records not matching our subdomain
+        if not cf_rec['name'].endswith(sub.lower() + '.' + config['cf-domain']):
+            continue
+
         if any(a['hostname'] == cf_name and a['address'] == cf_rec['content'] for a in ts_records):
             print("[{state}]: {host} -> {ip}".format(host=cf_rec['name'], ip=cf_rec['content'], state=colored("IN USE", "green")))
         else:
-            if (isTailscaleIP(cf_rec['content'])):
-                print("[{state}]: {host} -> {ip}".format(host=cf_rec['name'], ip=cf_rec['content'], state=colored('DELETING', "yellow")))
-                deleteDNSRecord(config['cf-key'], config['cf-domain'], cf_rec['id'])
-            else:
+            if (not isTailscaleIP(cf_rec['content'])):
                 print("[{state}]: {host} -> {ip} (IP does not belong to a tailscale host. please remove manualy)".format(host=cf_rec['name'], ip=cf_rec['content'], state=colored('SKIP DELETE', "red")))
+                continue
+
+            print("[{state}]: {host} -> {ip}".format(host=cf_rec['name'], ip=cf_rec['content'], state=colored('DELETING', "yellow")))
+            deleteDNSRecord(config['cf-key'], config['cf-domain'], cf_rec['id'])
+
+
+
 
 
 
